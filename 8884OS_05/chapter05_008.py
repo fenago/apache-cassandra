ee0d62e1c3f857deda0204c9a3b498bb4231fe3f
fenago <socrates73@gmail.com>
2022-02-22 14:52:20 +0500

    REMOVE 2021 LABS

--- 8884OS_05/chapter05_008.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# program: lab05_008.py
-
-## function to apply screening rule to generate buy signals
-## screening rule, Close > 10-Day SMA
-## d: DataFrame
-## return a DataFrame containing buy signals
-def signal_close_higher_than_sma10(d):
-    return d[d.close_price > d.sma]
